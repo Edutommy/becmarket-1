6d1da258d8f0aebfac71e8cbd8b5d0d49a8c9b71
BECMarket <83482302+BECMarket@users.noreply.github.com>
2021-07-14 03:31:23 -0400

    Update NuevoVendedor.js

--- js/NuevoVendedor.js
+++ js/NuevoVendedor.js
@@ -45,7 +45,7 @@ new Vue({
                     method:"post",
                     body:form,
                 });
-                const resp = await res.json();
+                const resp = await res.text();
                 console.log(resp);                
                 if (resp.msg == "si") {
                     window.location.href=this.url+'revision.html';
